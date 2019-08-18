dc0c3a53d84f45760637cc125e0b80ece7088b09
dev_dhkim <kim.dhdbr@gmail.com>
2019-08-18 19:32:19 +0900

    rebase 테스트 위한 소스 변경

--- Gitflow_Test/src/com/test/func/flo/MultipleClass.java
+++ Gitflow_Test/src/com/test/func/flo/MultipleClass.java
@@ -5,6 +5,7 @@ public class MultipleClass {
 	public float multipleFloat(float a, float b) {
 		// rebase 테스트를 위한develop브랜치의 소스 변경 
 		System.out.println("multiple float value");
+		System.out.println("renew test for multiple");
 		return a*b;
 	}
 }
