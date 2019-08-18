ec1d60b249640033e400d47a10c40f6d230907fc
dev_dhkim <kim.dhdbr@gmail.com>
2019-08-18 18:40:16 +0900

    develop 브랜치 로그 라인추가

--- Gitflow_Test/src/com/test/func/flo/PlusClass.java
+++ Gitflow_Test/src/com/test/func/flo/PlusClass.java
@@ -3,6 +3,8 @@ package com.test.func.flo;
 public class PlusClass {
 
 	public float addFloat(float a, float b) {
+		// develop 브랜치에 누군가 commit한 소스
+		System.out.println("debugging을 위한 로그 추가 ");
 		return a + b;
 	}
 }
