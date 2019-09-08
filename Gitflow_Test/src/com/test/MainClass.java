63127fe6c7448fb5818e793706344db8c2cd41f7
dev_dhkim <kim.dhdbr@gmail.com>
2019-09-08 22:09:42 +0900

    Generic test

--- Gitflow_Test/src/com/test/MainClass.java
+++ Gitflow_Test/src/com/test/MainClass.java
@@ -1,9 +1,22 @@
 package com.test;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.test.func.generic.GenericClassType;
+import com.test.func.generic.GenericTestClass;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-
+//		GenericClassType<String> genericType = new GenericClassType<String>("mesuri");
+//		genericType.print();
+		
+		List<String> userlist = new LinkedList<String>();
+		userlist.add("김수환무");
+		userlist.add("거북이와");
+		
+		GenericTestClass<List<String>> gen = new GenericTestClass<List<String>>(userlist);
 	}
 
 }
